4f5cd71a6e67699d0999bc9a3ef26bdbf4574007
dyson1602 <cclark000@citymail.cuny.edu>
2021-06-05 23:40:59 -0400

    new: class-based component of UserSearch

--- src/events/EventComponent.tsx
+++ src/events/EventComponent.tsx
@@ -1,16 +1,20 @@
+import React from 'react';
+
 const EventComponent: React.FC = () => {
   const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     console.log(event.target.value);
   };
 
-  const onDragStart = () => {
-    console.log("im being dragged")
-  }
+  const onDragStart = (event: React.DragEvent<HTMLDivElement>) => {
+    console.log('im being dragged');
+  };
 
   return (
     <div>
       <input onChange={onChange} />
-      <div draggable onDragStart={onDragStart}>Drag Me!</div>
+      <div draggable onDragStart={onDragStart}>
+        Drag Me!
+      </div>
     </div>
   );
 };
